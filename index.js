fed86b9e41b166195ff34eff8451572403ef83f5
Jirawat I <nodtem66@gmail.com>
2017-01-31 01:48:56 +0700

    support empty for Map, Set, and File (#7)
    * support empty for Map, Set, and File
    
    * Fixed #1
    * supported empty for Map, Set, and File
    * updated mocha and shouldjs to the latest version
    * added tests for Map, Set
    
    * support isempty for Error

--- index.js
+++ index.js
@@ -24,7 +24,17 @@ function isEmpty (val) {
   if (null == val) return true;
   if ('boolean' == typeof val) return false;
   if ('number' == typeof val) return 0 === val;
-  if (undefined !== val.length) return 0 === val.length;
+  if (val.toString) { 
+    if (
+      val.toString() === '[object Map]' ||
+      val.toString() === '[object Set]' ||
+      val.toString() === '[object File]'
+    ) return !val.size;
+    if (
+      val.toString().startsWith('Error')
+    ) return !val.message;      
+  }
   for (var key in val) if (has.call(val, key)) return false;
+  if (undefined !== val.length) return 0 === val.length;
   return true;
 }
