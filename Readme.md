bcfe3a5e648719750133cf3f4970fe776287d5f5
Jirawat I <nodtem66@gmail.com>
2017-01-30 21:29:13 +0700

    support empty for Map, Set, and File
    * Fixed #1
    * supported empty for Map, Set, and File
    * updated mocha and shouldjs to the latest version
    * added tests for Map, Set

--- Readme.md
+++ Readme.md
@@ -7,6 +7,7 @@
   
 ```
 $ npm install is-empty
+$ npm test
 ```
 
 ## Example
@@ -21,14 +22,18 @@ empty(0);               // true
 empty(function(){});    // true
 empty(null);            // true
 empty(undefined);       // true
+empty(new Map());       // true
+empty(new Set());       // true
 
-empty(true);			// false
-empty(false);			// false
+empty(true);			      // false
+empty(false);			      // false
 empty(['a', 'b']);      // false
 empty({ a: 'b' });      // false
 empty('string');        // false
 empty(42);              // false
 empty(function(a,b){}); // false
+empty(new Map([['key', 'value']])); // false
+empty(new Set([1]));    // false
 ```
 
 ## API
--- test/index.js
+++ test/index.js
@@ -4,38 +4,49 @@ var should = require('should');
 var empty = require('..');
 
 it('handles arrays', function () {
-  empty([]).should.be.true;
-  empty(['a', 'b']).should.be.false;
+  empty([]).should.be.true();
+  empty(['a', 'b']).should.be.false();
 });
 
 it('handles objects', function () {
-  empty({}).should.be.true;
-  empty({ a: 'b' }).should.be.false;
+  empty({}).should.be.true();
+  empty({ a: 'b' }).should.be.false();
+  empty({'length': 0}).should.be.false();
 });
 
 it('handles strings', function () {
-  empty('').should.be.true;
-  empty('string').should.be.false;
+  empty('').should.be.true();
+  empty('string').should.be.false();
 });
 
 it('handles numbers', function () {
-  empty(0).should.be.true;
-  empty(42).should.be.false;
+  empty(0).should.be.true();
+  empty(42).should.be.false();
 });
 
 it('handles functions', function () {
-  empty(function(){}).should.be.true;
-  empty(function(a,b){}).should.be.false;
+  empty(function(){}).should.be.true();
+  empty(function(a,b){}).should.be.false();
 });
 
 it('handles nulls', function () {
-  empty(null).should.be.true;
-  empty(undefined).should.be.true;
+  empty(null).should.be.true();
+  empty(undefined).should.be.true();
 });
 
 it('handles booleans', function () {
-  empty(false).should.be.false;
-  empty(true).should.be.false;
+  empty(false).should.be.false();
+  empty(true).should.be.false();
+});
+
+it('handles maps', function () {
+  empty(new Map()).should.be.true();
+  empty(new Map([['key', 'value']])).should.be.false();
+});
+
+it('handles sets', function () {
+  empty(new Set()).should.be.true();
+  empty(new Set([1,2,3,4])).should.be.false();
 });
 
 });
